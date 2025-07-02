e7e330ea5c754394f7e1e345ff0863b4c3b84f04
gnevesx <gnevestec@gmail.com>
2025-07-02 20:38:28 -0300

    Adiciona novos dominios de imagem para next/image

--- front_cliente_aula11/next.config.ts
+++ front_cliente_aula11/next.config.ts
@@ -3,9 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: [
-      'www.perdizesrigor.com.br', // Adicione este domínio aqui
-      // Se você tiver outras URLs de imagem de domínios externos no futuro,
-      // adicione-as nesta lista também.
+      'www.perdizesrigor.com.br',
+      'http2.mlstatic.com', // Mercado Livre
+      'static.zattini.com.br', // Zattini, Netshoes
+      'static.dafity.com.br', // Dafiti
+      'images.cea-ecommerce.com.br', // C&A
+      'assets.rnl.br', // Renner (ou similar, pode variar)
+      'hering.vtexassets.com', // Hering (domínio de CDN da Vtex)
+      'img.ltwebstatic.com', // Shein
+      'static.zara.net', // Zara
     ],
   },
   // Você pode adicionar outras configurações aqui no futuro, se precisar
